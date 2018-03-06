d321d18b16a860d2b7dbb126e1ddf8a0a19b3ffb
Luciano Oliveira <luciano101@msn.com>
2018-03-05 22:00:29 -0500

    add app.jsx adn index.jsx

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './src/index.jsx',
@@ -12,14 +12,14 @@ module.exports = {
         contentBase: './public',
     },
     resolve: {
-        extensions: ['', 'js', 'jsx'],
+        extensions: ['', '.js', '.jsx'],
         alias: {
             modules: __dirname + '/node_modules',
             jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
             bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
         }
     },
-    plugins: [
+    plugins: [ 
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
@@ -33,7 +33,7 @@ module.exports = {
             loader: 'babel-loader',
             exclude: /node_modules/,
             query: {
-                presets: ['es2015', 'react'],
+                presets: ['env', 'react'],
                 plugins: ['transform-object-rest-spread']
             }
         }, {
