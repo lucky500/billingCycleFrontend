8c780c811534fdacefa9fbede5e8420f37fe9b50
Luciano Oliveira <luciano101@msn.com>
2018-03-12 10:04:54 -0400

    add header component

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
             loader: 'babel-loader',
             exclude: /node_modules/,
             query: {
-                presets: ['env', 'react'],
+                presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
             }
         }, {
